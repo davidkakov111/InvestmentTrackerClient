5f900c3982df5c5b4edae66b2e9da1b70c9a6146
David Kovacs <143946579+davidkakov111@users.noreply.github.com>
2024-03-30 19:05:50 +0200

    sign out setup

--- src/Auth/SignOut.ts
+++ src/Auth/SignOut.ts
@@ -1,4 +1,12 @@
 export function SignOut() {
-  document.cookie = 'JWTToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'; 
+  // document.cookie = 'JWTToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'; 
+  // document.cookie = 'JWTToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.vercel.com;';
+  
+
+
+  document.cookie = `JWTToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.vercel.com; "secure;" : ""`;
+  
+
+
   window.location.href = '/signin';
 }
